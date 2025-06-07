138490c06995e7759b8e957bd9e2fcceb91361a6
bellopushon <75339433+bellopushon@users.noreply.github.com>
2025-06-07 18:07:03 -0400

    Fix Tailwind Config Syntax Error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-js/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: ['class', '.admin-dark'],
